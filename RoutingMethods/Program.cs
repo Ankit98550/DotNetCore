c0640bf606a0f9055fd12dbb4bc9c02c67e88ce2
Ankit98550 <ankit887gupta@gmail.com>
2023-12-27 10:58:58 +0530

    Action result learning

--- RoutingMethods/Program.cs
+++ RoutingMethods/Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 
 // to use routing u need to add routing middleware useRouting
 //after that call USEEndPoints method to define your enpoint such as MapGet MapPost MapPut MapDelete
-//app.UseRouting();
+app.UseRouting();
 //app.MapGet("/", () => "Hello World!");
 
 //mapping routing method 
@@ -19,31 +19,31 @@ var app = builder.Build();
 //    })
 //    }); 
 //this blog test with postman working fine
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapGet("/Home", async (con) =>
-//    {
-//        await con.Response.WriteAsync("this is Home Request comming From Get");
-//    });
-//    endpoints.MapPost("/Home", async (con) =>
-//    {
-//        await con.Response.WriteAsync("this is Home Request comming From Post");
-//    }); endpoints.MapPut("/Home", async (con) =>
-//    {
-//        await con.Response.WriteAsync("this is Home Request comming From Put");
-//    }); endpoints.MapDelete("/Home", async (con) =>
-//    {
-//        await con.Response.WriteAsync("this is Home Request comming From Delete");
-//    });
-//});
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/Home", async (con) =>
+    {
+        await con.Response.WriteAsync("this is Home Request comming From Get");
+    });
+    endpoints.MapPost("/Home", async (con) =>
+    {
+        await con.Response.WriteAsync("this is Home Request comming From Post");
+    }); endpoints.MapPut("/Home", async (con) =>
+    {
+        await con.Response.WriteAsync("this is Home Request comming From Put");
+    }); endpoints.MapDelete("/Home", async (con) =>
+    {
+        await con.Response.WriteAsync("this is Home Request comming From Delete");
+    });
+});
 //this blog test with postman working fine
 
 //without UseRouting running the functions
 
-app.MapGet("/Home", () => "Get Request");
-app.MapPost("/Home", () => "Get Post");
-app.MapPut("/Home", () => "Get Put");
-app.MapDelete("/Home", () => "Get Delete");
+//app.MapGet("/Home", () => "Get Request");
+//app.MapPost("/Home", () => "Get Post");
+//app.MapPut("/Home", () => "Get Put");
+//app.MapDelete("/Home", () => "Get Delete");
 
 
 
